546f0dc10c039c12bbe882352cebd5ccbee52bee
Fuzss <28218241+Fuzss@users.noreply.github.com>
2026-06-19 11:41:00 +0200

    replace ModDatapackRegistriesProvider

--- NeoForge/src/main/java/fuzs/bettertridents/neoforge/BetterTridentsNeoForge.java
+++ NeoForge/src/main/java/fuzs/bettertridents/neoforge/BetterTridentsNeoForge.java
@@ -1,11 +1,10 @@
 package fuzs.bettertridents.neoforge;
 
 import fuzs.bettertridents.common.BetterTridents;
-import fuzs.bettertridents.common.data.ModDatapackRegistriesProvider;
 import fuzs.bettertridents.common.data.ModRecipeProvider;
 import fuzs.bettertridents.common.data.loot.ModEntityInjectionLootProvider;
+import fuzs.bettertridents.common.init.ModRegistry;
 import fuzs.puzzleslib.common.api.core.v1.ModConstructor;
-import fuzs.puzzleslib.neoforge.api.data.v2.core.DataProviderHelper;
 import net.minecraft.server.packs.PackType;
 import net.neoforged.fml.common.Mod;
 
@@ -17,7 +16,7 @@ public class BetterTridentsNeoForge {
         DataProviderHelper.registerDataProviders(BetterTridents.MOD_ID, ModEntityInjectionLootProvider::new);
         DataProviderHelper.registerDataProviders(BetterTridents.BOOSTED_IMPALING_ID,
                 PackType.SERVER_DATA,
-                ModDatapackRegistriesProvider::new);
+                ModRegistry.REGISTRY_SET_BUILDER);
         DataProviderHelper.registerDataProviders(BetterTridents.TRIDENT_RECIPE_ID,
                 PackType.SERVER_DATA,
                 ModRecipeProvider::new);
